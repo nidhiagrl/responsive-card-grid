8742fbefd4b788a215ff48b9f1ba4d701c4bd6fa
nidhiagrl <nidhiagrl257@gmail.com>
2019-09-07 18:34:08 +0530

    Modal service formatted

--- src/app/modal/modal.service.ts
+++ src/app/modal/modal.service.ts
@@ -6,27 +6,23 @@ export class ModalService {
  private modals: any[] = [];
 
     add(modal: any) {
-        // add modal to array of active modals
         this.modals.push(modal);
         //console.log(this.modals);
     }
 
     remove(id: string) {
-        // remove modal from array of active modals
         let pos = this.modals.findIndex(x => x.id == id);
         this.modals.splice(pos,1);
     }
 
     open(id: string, obj) {
-        // open modal specified by id
         const modal = this.modals.find(x => x.id === id);
-        console.log("Here");
+        //console.log("Here");
         modal.open(obj);
     }
 
     close(id: string) {
-        // close modal specified by id
-        const modal = this.modals.find(x => x.id === id);
+       const modal = this.modals.find(x => x.id === id);
         modal.close();
     }
 }
